6d080544911cec40c239f189e3a11a492f9ffb27
suzu92 <92sustho@gafe.molndal.se>
2021-11-23 21:35:09 +0100

    Still fighting with input and total ticket count

--- script.js
+++ script.js
@@ -1,29 +1,16 @@
-/*let qtyInput = document.getElementsByClassName('qty-input')
-for (let i = 0; i < qtyInput.length; i++) {
-    let input = qtyInput[i]
-    input.addEventListener('change', qtyChange)
-}
-
-function qtyChange(event) {
-    let input = event.target
-    if (isNaN(input.value) || input.value < 0) {
-        input.value = 0
-    }
-}*/
-
+//minus och plus knappar
 let minusButton = document.getElementsByClassName('minus')
 let plusButton = document.getElementsByClassName('plus')
 for (let i = 0; i < minusButton.length; i++) {
     let button = minusButton[i]
-    button.addEventListener('click',function (event){
+    button.addEventListener('click', function (event) {
         let buttonClicked = event.target
         let input = buttonClicked.parentElement.children[1]
         let inputValue = input.value
         let newValue = parseInt(inputValue) - 1
         if (newValue >= 0) {
             input.value = newValue
-        }
-        else {
+        } else {
             input.value = 0
         }
         getTotalCount()
@@ -31,7 +18,7 @@ for (let i = 0; i < minusButton.length; i++) {
 }
 for (let i = 0; i < plusButton.length; i++) {
     let button = plusButton[i]
-    button.addEventListener('click',function (event){
+    button.addEventListener('click', function (event) {
         let buttonClicked = event.target
         let input = buttonClicked.parentElement.children[1]
         let inputValue = input.value
@@ -41,23 +28,28 @@ for (let i = 0; i < plusButton.length; i++) {
     })
 }
 
+//totalt antal biljetter
 function getTotalCount() {
-    let arr = document.querySelectorAll('.qty-input')
+    let tickets = document.querySelectorAll('.qty-input')
     let total = 0
-    for (let i = 0; i < arr.length; i++) {
-        if (parseInt(arr[i].value)) {
-            total += parseInt(arr[i].value)
+    for (let i = 0; i < tickets.length; i++) {
+        if (parseInt(tickets[i].value)) {
+            total += parseInt(tickets[i].value)
         }
     }
     document.getElementById('ticketAmount').value = total
 }
 
+function singleTotal() {
+    let a = Number(document.getElementsByClassName('qty-input').value)
+    let b = Number(document.getElementsByClassName('price').value)
+    let c = a * b
 
+    document.getElementsByClassName('ticket-total').value = c
+}
+singleTotal()
 
-
-
-
-/*function updateTicketTotal() {
+/*function grandTotal() {
     let ticketSection = document.getElementsByClassName('ticket')[0]
     let total = 0
     for (let i = 0; i < ticketSection.length; i++) {
@@ -70,10 +62,11 @@ function getTotalCount() {
     }
     document.getElementsByClassName('ticket-total')[0].innerText = total + ' kr'
 }
+grandTotal()*/
 
-updateTicketTotal()*/
 
 
+//popcorn över 500 kr
 function checkFreePopcorn(total) {
     if (total >= 500) {
         let freePopcorn = document.getElementById('free-Pop');
