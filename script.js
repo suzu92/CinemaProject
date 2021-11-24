ce4d4831b73d653c277d9bdf783e339ce271a882
suzu92 <92sustho@gafe.molndal.se>
2021-11-24 12:14:54 +0100

    My partners brother helped me with the total amount
    and from that I managed to set the individual total for each category

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+//biljettkostnader
+let priceOrdinary = 129
+let pricePensioner = 99
+let priceChildren = 99
+
+let amountOrdinary = 0
+let amountPensioner = 0
+let amountChildren = 0
+
 //minus och plus knappar
 let minusButton = document.getElementsByClassName('minus')
 let plusButton = document.getElementsByClassName('plus')
@@ -8,12 +17,28 @@ for (let i = 0; i < minusButton.length; i++) {
         let input = buttonClicked.parentElement.children[1]
         let inputValue = input.value
         let newValue = parseInt(inputValue) - 1
+        let getID = input.id
         if (newValue >= 0) {
             input.value = newValue
+            if (getID === 'q0' && newValue >= 0) {
+                amountOrdinary = newValue
+            } else if (getID === 'q1' && newValue >= 0) {
+                amountPensioner = newValue
+            } else if (getID === 'q2' && newValue >= 0) {
+                amountChildren = newValue
+            }
         } else {
             input.value = 0
+            if (getID === 'q0') {
+                amountOrdinary = 0
+            } else if (getID === 'q1') {
+                amountPensioner = 0
+            } else if (getID === 'q2') {
+                amountChildren = 0
+            }
         }
         getTotalCount()
+        totalAmount()
     })
 }
 for (let i = 0; i < plusButton.length; i++) {
@@ -22,13 +47,23 @@ for (let i = 0; i < plusButton.length; i++) {
         let buttonClicked = event.target
         let input = buttonClicked.parentElement.children[1]
         let inputValue = input.value
+        let getID = input.id
         input.value = parseInt(inputValue) + 1
 
+        if (getID === 'q0') {
+            amountOrdinary = parseInt(inputValue) + 1
+        } else if (getID === 'q1') {
+            amountPensioner = parseInt(inputValue) + 1
+        } else if (getID === 'q2') {
+            amountChildren = parseInt(inputValue) + 1
+        }
+
         getTotalCount()
+        totalAmount()
     })
 }
 
-//totalt antal biljetter
+//totalt antal biljetter samt totalsumman för varje kategori
 function getTotalCount() {
     let tickets = document.querySelectorAll('.qty-input')
     let total = 0
@@ -38,33 +73,17 @@ function getTotalCount() {
         }
     }
     document.getElementById('ticketAmount').value = total
+    document.getElementById('ticketAmountOrdinary').value = priceOrdinary * amountOrdinary
+    document.getElementById('ticketAmountPensioner').value = pricePensioner * amountPensioner
+    document.getElementById('ticketAmountChildren').value = priceChildren * amountChildren
 }
+//totalen av alla biljettpriser
+function totalAmount() {
+    let grandTotal = (amountChildren * priceChildren) + (amountOrdinary * priceOrdinary) + (amountPensioner * pricePensioner)
 
-function singleTotal() {
-    let a = Number(document.getElementsByClassName('qty-input').value)
-    let b = Number(document.getElementsByClassName('price').value)
-    let c = a * b
-
-    document.getElementsByClassName('ticket-total').value = c
+    document.getElementById('grandTotal').textContent = grandTotal
+    checkFreePopcorn(grandTotal)
 }
-singleTotal()
-
-/*function grandTotal() {
-    let ticketSection = document.getElementsByClassName('ticket')[0]
-    let total = 0
-    for (let i = 0; i < ticketSection.length; i++) {
-        let ticketSection = ticketSection[i]
-        let ticketPrice = ticketSection[i].getElementsByClassName('price')[0]
-        let ticketQty = ticketSection[i].getElementsByClassName('qty-input')[0]
-        let price = parseInt(ticketPrice.innerText.replace('kr', ''))
-        let qty = ticketQty.innerText
-        total += (price * qty)
-    }
-    document.getElementsByClassName('ticket-total')[0].innerText = total + ' kr'
-}
-grandTotal()*/
-
-
 
 //popcorn över 500 kr
 function checkFreePopcorn(total) {
@@ -82,4 +101,14 @@ function checkFreePopcorn(total) {
     }
 }
 
-checkFreePopcorn()
+//order button
+document.querySelector('.order-btn').addEventListener('click', checkoutMessage);
+
+function checkoutMessage() {
+    let totalSum = document.getElementById('grandTotal').innerText;
+    if (totalSum === 0) {
+        alert('You have no ordered any tickets yet');
+    } else {
+        alert('Thank you for your order! Hope you enjoy the movie!');
+    }
+}
